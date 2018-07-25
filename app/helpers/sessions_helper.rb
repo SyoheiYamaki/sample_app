bdb91e4f7c02e2ef278e8059b3441ec7e0d723c9
山木祥平 <yamaki817122@gmail.com>
2018-07-26 00:22:19 +0900

    Finish user edit, update,index,,,

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,11 @@ module SessionsHelper
 		end			
 	end
 
+	# 渡されたユーザーがログイン済みユーザーであればtrueを返す
+	def current_user?(user)
+		user == current_user
+	end
+
 	def logged_in?
 		!current_user.nil?
 	end
@@ -38,5 +43,15 @@ module SessionsHelper
 		cookies.delete(:user_id)
 		cookies.delete(:remember_token)
 	end
+
+	  def redirect_back_or(default)
+	    redirect_to(session[:forwarding_url] || default)
+	    session.delete(:forwarding_url)
+	  end
+
+	  # アクセスしようとしたURLを覚えておく
+	  def store_location
+	    session[:forwarding_url] = request.original_url if request.get?
+	  end
 
 end
